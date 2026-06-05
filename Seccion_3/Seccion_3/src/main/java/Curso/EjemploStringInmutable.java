a9f07ba3a673926c031481489af764c23663fb53
Samuel Abdali Mosso Flores <samuelisramosso@gmail.com>
2026-06-05 01:03:19 -0600

    Metodos importantes clase String

---
+++ Seccion_3/Seccion_3/src/main/java/Curso/EjemploStringInmutable.java
@@ -0,0 +1,26 @@
+package Curso;
+
+public class EjemploStringInmutable {
+
+    public static void main(String[] args) {
+        String curso= "Programacion Java";
+        String profesor= "Andres Guzman";
+
+        String resultado= curso.concat(" con ").concat(profesor);
+        System.out.println("Curso: "+curso);
+        System.out.println(resultado);
+        System.out.println(curso==resultado);
+
+        String resultado2 =curso.transform(c ->{
+            return c + " con " + profesor;
+        });
+        System.out.println("Curso = "+curso);
+        System.out.println("resultado2 "+resultado2);
+        String resultado3=resultado.replace("a","A");
+        System.out.println("resultado "+resultado);
+
+        System.out.println("resultado3 "+resultado3);
+
+
+    }
+}
